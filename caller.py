4137b48be95960ac1fc2a528d7cedf7c67a11ed5
Ryosuke Morino <morinor@wit.edu>
2014-01-13 01:28:10 -0500

    20140113

---
+++ caller.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+import wrapper
+import subprocess
+
+##VARIABLES
+#Github API base url
+base_url = 'https://api.github.com/users/'
+#url = 'https://api.github.com/users/chasiumen/repos'
+#Github repo to push
+repo = 'https://github.com/chasiumen/github-api-testing.git'
+#working directory
+wdir = '/Users/leox/github/github-api-testing/'
+#commit command
+date ='date +%Y%m%d'
+proc= subprocess.Popen([date], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+(save, err) = proc.communicate()
+save = save.strip()
+ccmd = 'git commit -a --allow-empty-message -m \'' + save + '\''
+#push to git
+cmd = 'git push'
+
+
+##FUNCTION: Runs shell commads
+def push2Github(cmd):
+#    print cmd
+    proc = subprocess.Popen([cmd], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    (r, err) = proc.communicate()
+    return r
+
+
+##USER INPUT
+print 'Access to Github'
+uin  = raw_input("Enter Github User ID>")
+user = uin + '/'
+url = base_url+user+'repos'
+
+#print url
+
+a = wrapper.git_api('test')
+a.RepoNames(url)
+a.totalNumberOfRepos(url)
+
+##Github upload
+add = push2Github('git add'+ wdir)
+print add
+commit = push2Github(ccmd)
+r  = push2Github(cmd)
---
+++ wrapper.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+import requests
+import json
+import sys
+
+class git_api:
+
+    def __init__(self, name):
+        self.__name__ = name
+        print 'Created git_api successfully'
+    def RepoNames(self, url):
+        r = requests.get(url)
+        if (r.ok):
+            data = json.loads(r.text or r.content)
+            print '==========List of the Repos=========='
+            print '(' + url + ')'
+            #json_out = json.dumps(data, sort_keys=True, indent=4)
+            for i in range(0, len(data)):
+                print data[i]['name']
+        else:
+            print "Error: Unreachable to the host", 'url'
+            sys.exit()
+    def totalNumberOfRepos(self, url):
+        r = requests.get(url)
+        if (r.ok):
+            data = json.loads(r.text or r.content)
+            print 'Total number of Repo: ' + '[' +  str(len(data)) + ']'
+
+
+#url = 'https://api.github.com/users/chasiumen/repos'
+#a = git_api('test')
+#a.RepoNames(url)
+#a.totalNumberOfRepos(url)
